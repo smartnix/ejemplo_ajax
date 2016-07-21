a640916e9a5c093c85a86335732025bdec17ae2c
Santiago <smartnix@hotmail.com>
2016-07-21 17:36:11 -0500

    proyecto con instalacion de dependencias

--- js/ajax.js
+++ js/ajax.js
@@ -1,5 +1,14 @@
 var request = new XMLHttpRequest();
 
-function loadTxt(params) {
-    
+function loadTxt() {
+    request.onreadystatechange = processTxt(); /*Cada ves que halla un cambio de estado */
+    request.open("GET", "data.txt", true); /*true en segundo plano */
+    request.send();
+}
+
+function processTxt() {
+    if (request.status == 200 && request.readyState == 4) {
+        var txt = document.getElementById("txt");
+        txt.innerHTML = request.responseText;
+    }
 }
