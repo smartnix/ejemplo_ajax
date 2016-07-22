99e3eef04944d1e46de165cb7b581d8622e38810
Santiago <smartnix@hotmail.com>
2016-07-21 19:38:58 -0500

    se cargo el servidor

--- js/ajax.js
+++ js/ajax.js
@@ -1,7 +1,8 @@
 var request = new XMLHttpRequest();
+var requestUsr = new XMLHttpRequest();
 
 function loadTxt() {
-    request.onreadystatechange = processTxt(); /*Cada ves que halla un cambio de estado */
+    request.onreadystatechange = processTxt; /*Cada ves que halla un cambio de estado */
     request.open("GET", "data.txt", true); /*true en segundo plano */
     request.send();
 }
@@ -10,5 +11,27 @@ function processTxt() {
     if (request.status == 200 && request.readyState == 4) {
         var txt = document.getElementById("txt");
         txt.innerHTML = request.responseText;
+    }
+}
+
+function loadUsr() {
+    requestUsr.onreadystatechange = processUsr;
+    requestUsr.open("GET", "usuarios.json", true);
+    requestUsr.send();
+}
+
+function processUsr() {
+    if (requestUsr.status == 200 && requestUsr.readyState == 4) {
+        var data = requestUsr.responseText;
+        data = JSON.parse(data);
+        var usr = document.getElementById("usr");
+        usr.innerHTML = "";
+        for (var i = 0; i < data.length; i++) {
+            var u = data[i];
+            usr.innerHTML += "<h1>" + u.nombre + "</h1>"
+                + "<p>" + u.celular + "</p>"
+                + "<p>" + u.correo + "</p>"
+                + "<hr>";
+        }
     }
 }
